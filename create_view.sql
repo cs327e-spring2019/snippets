e83682c2df7c638de46508d224f47507d34c7d89
Shirley Cohen <scohen@cs.utexas.edu>
2019-03-12 11:05:04 -0500

    sample view

--- create_view.sql
+++ create_view.sql
@@ -1,10 +1,13 @@
--- sample view for Milestone 7
-
+-- create view example for Milestone 7
+-- note: the project id is required when creating views
 create view oscars.oscar_nominations as
 select entity, count(*) as nomination_count
 from `cs327e-sp2019.oscars.Academy_Award` 
 where category in ('ACTOR IN A LEADING ROLE', 'ACTOR IN A SUPPORTING ROLE', 'ACTRESS', 'ACTRESS IN A LEADING ROLE', 
-'ACTRESS IN A SUPPORTING ROLE', 'DIRECTING (Comedy Picture)', 
-'DIRECTING (Dramatic Picture)')
+'ACTRESS IN A SUPPORTING ROLE', 'DIRECTING (Comedy Picture)', 'DIRECTING (Dramatic Picture)')
 group by entity
 order by nomination_count desc
+
+-- query the view as follows: 
+-- select * from `cs327e-sp2019.oscars.oscar_nominations`
+-- note: you must qualify the fully qualify the view with its project id and dataset. 
