2d94a18036b69b8194371d431cbbff4dda7c9e06
William <2899523@gmail.com>
2019-03-29 03:10:05 -0500

    created another midterm review file

---
+++ mt_review2.py
@@ -0,0 +1,42 @@
+import os
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+
+class Lmfao(beam.DoFn):
+  def process(self, element):
+    employed_at = element.get('employed_at')
+    salary = element.get('salary')
+    return [(employed_at, salary)]
+    
+class Rofl(beam.DoFn):
+  def process(self, element):
+    employed_at = element[0]
+    salaries    = element[1]
+    return {
+      'employed_at'  : employed_at
+      'mystery_field': sum(salaries)/len(salaries)
+    }
+         
+PROJECT_ID = os.environ['PROJECT_ID']
+
+options = { 'project': PROJECT_ID }
+opts = beam.pipeline.PipelineOptions(flags=[], **options)
+
+with beam.Pipeline('DirectRunner', options=opts) as p:
+
+    query_results = p | 'Read from BigQuery' >> beam.io.Read(beam.io.BigQuerySource(query='SELECT employed_at, salary, fname, lname FROM fast_food.Employee'))
+
+    actor_pcoll = query_results | 'Awesome Transform' >> beam.ParDo(Lmfao())
+
+    group_pcoll = actor_pcoll | 'Rad Transform' >> beam.GroupByKey()
+  
+    sum_pcoll = group_pcoll | 'Cool Transform' >> beam.ParDo(Rofl())
+    
+    qualified_table_name = PROJECT_ID + ':fast_food.MysteryTable'
+    table_schema = 'employed_at:INTEGER,mystery_field:NUMERIC'
+    
+    out_pcoll | 'Write to BigQuery' >> beam.io.Write(beam.io.BigQuerySink(qualified_table_name, 
+                                                    schema=table_schema,  
+                                                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+                                                    write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
